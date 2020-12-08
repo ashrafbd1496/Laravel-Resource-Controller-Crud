5a737cf60d1f69ad49e5518f21572c70cb3097a3
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-08 23:49:09 +0600

    'table migrated' newly migrated with the name patient

--- database/migrations/2020_12_08_174056_create_patients_table.php
+++ database/migrations/2020_12_08_174056_create_patients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHospitalsTable extends Migration
+class CreatePatientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateHospitalsTable extends Migration
      */
     public function up()
     {
-        Schema::create('hospitals', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email') ->unique();
-            $table->string('cell')->unique();
-            $table->string('uname')->unique();
-            $table->string('photo')->nullable()->default('avatar.jpg');
+            $table->string('cell') ->unique();
+            $table->string('username') ->unique();
+            $table->string('photo') ->nullable() ->default('avatar.jpg');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateHospitalsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('hospitals');
+        Schema::dropIfExists('patients');
     }
 }
